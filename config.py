1f719c5d39febed118c5ea11dbb3b77e9f24d955
sreeharizenturiotech-ai <sreehari.zenturiotech@gmail.com>
2026-02-09 11:50:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 CHUNK_SIZE = 300
 CHUNK_OVERLAP = 50
 
-YOUTUBE_PATH = "data/merged_youtube.json"
-WEBSITE_PATH = "data/merged.json"
+YOUTUBE_PATH = "datas/merged_youtube.json"
+WEBSITE_PATH = "datas/merged.json"
 
 RAG_DOCS_PATH = "outputs/rag_documents.json"
 FAISS_INDEX_PATH = "outputs/rag_index.faiss"
