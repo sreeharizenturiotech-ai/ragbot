139edcf277b56f70d75b708bb35465afd9e63ab3
sreeharizenturiotech-ai <sreehari.zenturiotech@gmail.com>
2026-02-12 13:49:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ WEBSITE_PATH = "datas/merged.json"
 
 RAG_DOCS_PATH = "outputs/rag_documents.json"
 FAISS_INDEX_PATH = "outputs/rag_index.faiss"
+NEW_FAISS_INDEX="outputs/new_rag_index.faiss"
 
 MODEL_NAME = "google/gemma-2-2b-it"
